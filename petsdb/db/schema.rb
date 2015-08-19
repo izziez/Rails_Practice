6bcba0452ae0796e3874dc80d564f73a23b143f6
izziez <izzie@bizziecreative.com>
2015-08-19 12:09:19 -0400

    'add employee table'

--- petsdb/db/schema.rb
+++ petsdb/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819152149) do
+ActiveRecord::Schema.define(version: 20150819155851) do
 
   create_table "clubs", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "club_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
